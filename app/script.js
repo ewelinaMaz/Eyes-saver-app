302fa382f934c780289afff844e88df21ded3318
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-05-30 12:59:57 +0200

    Add correct time

--- app/script.js
+++ app/script.js
@@ -2,79 +2,80 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
-constructor(props){
+  constructor(props) {
     super(props)
     this.state = { status: 'off', time: 0, timer: null }
-}
+  }
 
-formatTime = (Totalseconds) => {
-const minutes = Math.floor(Totalseconds / 60);
-const seconds = Math.floor(Totalseconds - minutes * 60);
+  formatTime = (Totalseconds) => {
+    const minutes = Math.floor(Totalseconds / 60);
+    const seconds = Math.floor(Totalseconds - minutes * 60);
 
-const countDown = `${(minutes < 10 ? `0${minutes}` : minutes)}:${(seconds < 10 ? `0${seconds}` : seconds)}`;
-return countDown;
-} 
+    const countDown = `${(minutes < 10 ? `0${minutes}` : minutes)}:${(seconds < 10 ? `0${seconds}` : seconds)}`;
+    return countDown;
+  }
 
-step = () => {
+  step = () => {
     this.setState({
-        time: this.state.time -1});
-        if(this.state.time === 0) {
-            this.setState({
-                status: this.state.status === 'work' ? 'rest' : 'work',
-                time: this.state.status === 'work' ? 20 : 40
-            });
-            this.playBell();
-        }
-};
+      time: this.state.time - 1
+    });
+    if (this.state.time === 0) {
+      this.setState({
+        status: this.state.status === 'work' ? 'rest' : 'work',
+        time: this.state.status === 'work' ? 20 : 1200
+      });
+      this.playBell();
+    }
+  };
 
-playBell = () => {
+  playBell = () => {
     const bell = new Audio('./sounds/bell.wav');
     bell.play();
   };
 
-startTimer = () => {
+  startTimer = () => {
     this.setState({
-        timer:setInterval( ()=> this.step(), 1000),
-        time: 40,
-        status: 'work'
+      timer: setInterval(() => this.step(), 1000),
+      time: 1200,
+      status: 'work'
     });
-};
-stopTimer = () => {
+  };
+  stopTimer = () => {
     this.setState({
-        timer: null,
-        time: 0,
-        status: 'off'
+      timer: null,
+      time: 0,
+      status: 'off'
     });
-};
-closeApp = () => {
+  };
+  closeApp = () => {
     window.close();
-};
+  };
 
-componentWillUnmount() {
+  componentWillUnmount() {
     clearInterval(this.timeInterval)
-};
+  };
   render() {
-    const {status, time} = this.state;
+    const { status, time } = this.state;
     return (
       <div>
         <h1>Protect your eyes</h1>
-        {( status === 'off') &&
-        <div className="App description">
-        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
-        <p>This app will help you track your time and inform you when it's time to rest.</p>
-        </div>}
-        {( status === 'work') &&
-        <img className='work' src="./images/work.png" />}
-        {( status === 'rest') &&
-        <img className='rest' src="./images/rest.png" />}
-        {( status !== 'off') &&
-        <div className="timer">
-          {this.formatTime(time)}
-        </div>}
-        {( status === 'off') &&
-        <button onClick={this.startTimer} className="btn">Start</button>}
-        {( status !== 'off') &&
-        <button onClick={this.stopTimer} className="btn">Stop</button>}
+        {(status === 'off') &&
+          <div className="App description">
+            <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+            <p>This app will help you track your time and inform you when it's time to rest.</p>
+          </div>}
+        {(status === 'work') &&
+          <img className='work' src="./images/work.png" />}
+        {(status === 'rest') &&
+          <img className='rest' src="./images/rest.png" />}
+        {(status !== 'off') &&
+          <div className="timer">
+            {this.formatTime(time)}
+          </div>}
+        {(status === 'off') &&
+          <button onClick={this.startTimer} className="btn">Start</button>}
+        {(status !== 'off') &&
+          <button onClick={this.stopTimer} className="btn">Stop</button>}
         <button onClick={this.closeApp} className="btn btn-close">X</button>
       </div>
     )
